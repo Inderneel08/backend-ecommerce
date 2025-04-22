6873bda15afbe1be4a3aa3cb16bf43783b4dbdbd
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-22 11:14:19 +0530

    Update CategoryController.java

--- src/main/java/com/example/backend_ecommerce/Controllers/CategoryController.java
+++ src/main/java/com/example/backend_ecommerce/Controllers/CategoryController.java
@@ -1,15 +1,21 @@
 package com.example.backend_ecommerce.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend_ecommerce.ServiceLayer.CategoryServiceLayer;
+
 @RestController
 public class CategoryController {
 
+    @Autowired
+    private CategoryServiceLayer categoryServiceLayer;
+
     @GetMapping("/api/getAll/categories")
     public ResponseEntity<?> getAllCategories() {
-
+        return(ResponseEntity.ok().body(categoryServiceLayer.getAllCategories()));
     }
 
 }
