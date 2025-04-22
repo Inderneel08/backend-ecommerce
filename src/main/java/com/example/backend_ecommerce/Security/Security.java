b9229ed5821ee1e2373177dfc84464b8cf326212
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-22 11:26:08 +0530

    Update Security.java

--- src/main/java/com/example/backend_ecommerce/Security/Security.java
+++ src/main/java/com/example/backend_ecommerce/Security/Security.java
@@ -3,6 +3,7 @@ package com.example.backend_ecommerce.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,10 +11,11 @@ public class Security {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        httpSecurity.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/login", "/api/auth/register","/api/getStates","/api/products","/api/products/category","/api/getAll/categories").permitAll()
                 .anyRequest().authenticated())
-                .formLogin().disable();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(form -> form.disable());
 
         return (httpSecurity.build());
     }
