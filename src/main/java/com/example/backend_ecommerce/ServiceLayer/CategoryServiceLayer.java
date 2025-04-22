b348e1456fb491b1bb0792a1779c9bd210995b39
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-22 17:13:06 +0530

    Update CategoryServiceLayer.java

--- src/main/java/com/example/backend_ecommerce/ServiceLayer/CategoryServiceLayer.java
+++ src/main/java/com/example/backend_ecommerce/ServiceLayer/CategoryServiceLayer.java
@@ -2,12 +2,9 @@ package com.example.backend_ecommerce.ServiceLayer;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.backend_ecommerce.Models.Category;
-import com.example.backend_ecommerce.Models.Products;
 import com.example.backend_ecommerce.RepositoryLayer.CategoryRepository;
 
 @Service
