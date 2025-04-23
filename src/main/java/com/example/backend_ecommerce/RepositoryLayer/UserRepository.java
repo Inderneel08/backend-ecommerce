94b370731d56c27b7300264065b950fb416329c3
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-23 17:02:55 +0530

    Update UserRepository.java

--- src/main/java/com/example/backend_ecommerce/RepositoryLayer/UserRepository.java
+++ src/main/java/com/example/backend_ecommerce/RepositoryLayer/UserRepository.java
@@ -3,9 +3,13 @@ package com.example.backend_ecommerce.RepositoryLayer;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.backend_ecommerce.Models.Users;
 
 public interface UserRepository extends JpaRepository<Users,BigInteger>{
     
+    @Query(value = "SELECT * FROM users where email = :email",nativeQuery = true)
+    public Users findByemail(@Param("email") String email);
 }
