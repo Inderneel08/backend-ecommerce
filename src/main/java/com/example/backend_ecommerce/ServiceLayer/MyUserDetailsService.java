6f4b43e54ec77c6b890048e129bf37584811529c
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-23 11:04:04 +0530

    Update MyUserDetailsService.java

--- src/main/java/com/example/backend_ecommerce/ServiceLayer/MyUserDetailsService.java
+++ src/main/java/com/example/backend_ecommerce/ServiceLayer/MyUserDetailsService.java
@@ -2,19 +2,28 @@ package com.example.backend_ecommerce.ServiceLayer;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.backend_ecommerce.Models.Users;
+import com.example.backend_ecommerce.RepositoryLayer.UserRepository;
+
 @Service
 public class MyUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository userRepository;
     
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+
+        Users users = userRepository.findByemail(username);
+
+        return(users);
     }
 
     public boolean createUser(@RequestBody Map<String,Object> body)
