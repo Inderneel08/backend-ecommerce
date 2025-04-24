37e6c864c3978e322be5e2b5439486f537f86297
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 11:04:15 +0530

    Update TokenRepository.java

--- src/main/java/com/example/backend_ecommerce/RepositoryLayer/TokenRepository.java
+++ src/main/java/com/example/backend_ecommerce/RepositoryLayer/TokenRepository.java
@@ -3,6 +3,7 @@ package com.example.backend_ecommerce.RepositoryLayer;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,5 +13,9 @@ public interface TokenRepository extends JpaRepository<TokenTable,BigInteger>{
 
     @Query(value = "SELECT * FROM token_table where token_table.token_value = :token_value",nativeQuery = true)
     public TokenTable findByTokenValue(@Param("token_value") String token_value);
+
+    @Modifying
+    @Query(value = "DELETE FROM token_table where token_table.token_value = :token_value",nativeQuery = true)
+    public void deleteToken(@Param("token_value") String token_value);
 
 }
