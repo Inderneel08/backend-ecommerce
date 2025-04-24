fba289884ad3fc08b5c7259dfd9baadfeafb1823
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 11:02:56 +0530

    Update Security.java

--- src/main/java/com/example/backend_ecommerce/Security/Security.java
+++ src/main/java/com/example/backend_ecommerce/Security/Security.java
@@ -3,6 +3,7 @@ package com.example.backend_ecommerce.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.example.backend_ecommerce.Components.JwtFilter;
 
 @Configuration
+@EnableMethodSecurity
 public class Security {
 
     @Autowired
