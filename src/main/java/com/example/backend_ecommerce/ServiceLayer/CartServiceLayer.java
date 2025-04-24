140556d2c0e79995094862701790855bfcef6079
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 16:26:49 +0530

    Create CartServiceLayer.java

---
+++ src/main/java/com/example/backend_ecommerce/ServiceLayer/CartServiceLayer.java
@@ -0,0 +1,12 @@
+package com.example.backend_ecommerce.ServiceLayer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceLayer {
+    
+    public Integer cartCount()
+    {
+
+    }
+}
