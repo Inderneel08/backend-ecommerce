7f8424eb39fd28030fbc36613be2d915bfc65aa7
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 12:11:26 +0530

    Update UserRepository.java

--- src/main/java/com/example/backend_ecommerce/RepositoryLayer/UserRepository.java
+++ src/main/java/com/example/backend_ecommerce/RepositoryLayer/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.backend_ecommerce.RepositoryLayer;
 import java.math.BigInteger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,8 @@ public interface UserRepository extends JpaRepository<Users,BigInteger>{
     
     @Query(value = "SELECT * FROM users where email = :email",nativeQuery = true)
     public Users findByemail(@Param("email") String email);
+
+    @Modifying
+    @Query(value = "UPDATE users set users.password = :password where users.id = :userId",nativeQuery = true)
+    public void savePassword(@Param("userId") BigInteger userId,@Param("password") String password);
 }
