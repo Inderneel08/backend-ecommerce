056e99aa516720b576810ff39293fc719c8fcb05
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 11:02:28 +0530

    Update LoginController.java

--- src/main/java/com/example/backend_ecommerce/Controllers/LoginController.java
+++ src/main/java/com/example/backend_ecommerce/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.example.backend_ecommerce.Controllers;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import com.example.backend_ecommerce.Models.Users;
 import com.example.backend_ecommerce.ServiceLayer.JwtTokenServiceLayer;
 import com.example.backend_ecommerce.ServiceLayer.MyUserDetailsService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
@@ -43,4 +45,26 @@ public class LoginController {
 
         return (ResponseEntity.ok().build());
     }
+
+    // {
+    //     "email":"test123@yopmail.com",
+    //     "password":"5a5d3e1115b0bae8e32a610d20390f818a5ec81c90f6cf8c1a0be1b3c626b975"
+    //   }
+
+    @PreAuthorize("hasRole('USER')")
+    @PostMapping("/api/auth/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request,HttpServletResponse response)
+    {
+        if(!jwtTokenServiceLayer.deleteToken((String)request.getAttribute("hashedToken"))){
+            return(ResponseEntity.badRequest().body("Some error happened!"));
+        }
+
+        Cookie cookie = new Cookie("token", null); // Clear the token
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+
+        return(ResponseEntity.ok().body("Logout successfull!"));
+    }
+
 }
