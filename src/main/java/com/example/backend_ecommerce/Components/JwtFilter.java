bb1bf46bf57229bb4cac0cace23fe5e3b58b7905
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-24 11:02:04 +0530

    Update JwtFilter.java

--- src/main/java/com/example/backend_ecommerce/Components/JwtFilter.java
+++ src/main/java/com/example/backend_ecommerce/Components/JwtFilter.java
@@ -2,20 +2,56 @@ package com.example.backend_ecommerce.Components;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.example.backend_ecommerce.ServiceLayer.JwtTokenServiceLayer;
+import com.example.backend_ecommerce.ServiceLayer.MyUserDetailsService;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
 
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtTokenServiceLayer jwtTokenServiceLayer;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+    
+        String token = null;
+
+        if(request.getCookies()!=null){
+            for (Cookie cookie : request.getCookies()) {
+                if ("token".equals(cookie.getName())) {
+                    token = cookie.getValue();
+                }
+            }
+        }
+
+        if(token!=null){
+            String hashedToken = jwtTokenServiceLayer.findToken(token);
+
+            request.setAttribute("hashedToken", hashedToken);
+
+            UserDetails userDetails = myUserDetailsService.loadUserByUsername(jwtTokenServiceLayer.findEmailByToken(hashedToken));
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,null, userDetails.getAuthorities());
+            
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
 
         filterChain.doFilter(request, response);
     }
