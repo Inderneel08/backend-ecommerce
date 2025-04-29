1036cdca17473b7384a63bd0d57cee99542cf4f0
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-04-29 22:54:09 +0530

    04/29/2025

--- src/main/java/com/example/backend_ecommerce/Controllers/PaymentController.java
+++ src/main/java/com/example/backend_ecommerce/Controllers/PaymentController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,11 @@ public class PaymentController {
         }
 
         return (ResponseEntity.ok().body(orders.getPayment_session_id()));
+    }
+
+    @GetMapping("/api/processPendingOrders")
+    public ResponseEntity<?> processPendingOrders() {
+        return (ResponseEntity.ok().build());
     }
 
 }
