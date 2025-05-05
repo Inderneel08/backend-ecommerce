7cd03de849af6b1f1f13bd59b1a8fdbaf2b5f354
Inderneel08 <68139492+Inderneel08@users.noreply.github.com>
2025-05-05 16:22:58 +0530

    Update ProductRepository.java

--- src/main/java/com/example/backend_ecommerce/RepositoryLayer/ProductRepository.java
+++ src/main/java/com/example/backend_ecommerce/RepositoryLayer/ProductRepository.java
@@ -14,8 +14,4 @@ public interface ProductRepository extends JpaRepository<Products, BigInteger> {
 
     @Query(value = "SELECT * FROM products where products.category = :id", nativeQuery = true)
     List<Products> findByCategory(@Param("id") Integer id);
-
-    @Query(value = "SELECT products.id as product_id,products.title,products.price,products.description,products.image,rating.rate,category.id as category_id,category.category_name FROM products inner join rating on products.rate_id=rating.id inner join category on products.category=category.id where products.id = :id",nativeQuery = true)
-    List<ProductInfoDTO> getProductInfo(@Param("id") BigInteger id);
-
 }
